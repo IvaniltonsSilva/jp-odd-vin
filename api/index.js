b6fc9088315ac68971980f63c756f0a43a7bb0cf
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-25 13:18:35 -0300

    chore: update shareCode to Y1WT1QCRWAD

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=ZWKWCJL0SFW&afp=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=Y1WT1QCRWAD&afp=oa' });
     res.end();
   }
